09e82b6d1d988aaa99ff85b0c0c22aebd81e42e6
Ze-hs <thehao33@gmail.com>
2025-06-18 11:55:33 -0400

    Able to draw rectangles using mouse

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,92 @@
-import { useLayoutEffect, useState } from 'react';
-import './App.css'
+import { useLayoutEffect, useState } from "react";
+import "./App.css";
 
-import rough from 'roughjs';
+import rough from "roughjs";
+import type { Drawable } from "roughjs/bin/core";
+
+interface DrawableElem {
+	x1: number;
+	y1: number;
+	x2: number;
+	y2: number;
+	roughElem: Drawable;
+}
 
 const App = () => {
-    const [drawing, setDrawing] = useState(false);
-    
-    // Might need to move this outside
+	const [drawing, setDrawing] = useState(false);
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [elems, setElems] = useState<DrawableElem[]>([]);
 
-    useLayoutEffect( () => {
-        const canvas = document.getElementById('canvas');
-        if (canvas instanceof HTMLCanvasElement) {
-            const ctx = canvas.getContext('2d');
+	// ----------------- Might need to move this outside or useref
+	const gen = rough.generator();
 
-            // Clear the canvas
-            ctx?.clearRect(0,0,canvas.width, canvas.height);
+	const createElement = (
+		x1: number,
+		y1: number,
+		x2: number,
+		y2: number
+	): DrawableElem => {
+		const roughElem = gen.rectangle(x1, y1, x2 - x1, y2 - y1);
+		return { x1, y1, x2, y2, roughElem };
+	};
 
-            canvas.width = canvas.clientWidth;
-            canvas.height = canvas.clientHeight;
+	// -----------------
 
-            const roughCanvas = rough.canvas(canvas);
-            roughCanvas.rectangle(10,10,150,150);
-        }
-    }, [])
+	useLayoutEffect(() => {
+		const canvas = document.getElementById("canvas");
+		if (canvas instanceof HTMLCanvasElement) {
+			const ctx = canvas.getContext("2d");
 
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
 
-    const handleMouseDown = () => {
-        setDrawing(true);
-    };
-    
-    const handleMouseUp = () => {
-        setDrawing(false);
-    };
+			// Clear the canvas
+			ctx?.clearRect(0, 0, canvas.width, canvas.height);
 
-    const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
-        if (!drawing) {
-            return;
-        }
+			const roughCanvas = rough.canvas(canvas);
+			
+            elems.forEach( elem => {
+                roughCanvas.draw(elem.roughElem)
+            })
 
-        const {clientX, clientY} = event;
-        console.log(clientX, clientY);
-    };
+		}
+	},[elems]);
 
-    return (
-        <canvas 
-            id="canvas" 
-            onMouseDown={handleMouseDown}
-            onMouseUp={handleMouseUp}
-            onMouseMove={handleMouseMove}
-        ></canvas>
-    )
-}
+	const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+		setDrawing(true);
+		const { clientX, clientY } = event;
+		const roughtElem = createElement(clientX, clientY, clientX, clientY);
+		setElems(elems.concat(roughtElem));
+        console.log(clientX, clientY)
+	};
+
+	const handleMouseUp = () => {
+		setDrawing(false);
+	};
+
+	const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+		if (!drawing) {
+			return;
+		}
+
+		const { clientX, clientY } = event;
+		const tempElem = [...elems];
+		const index = tempElem.length - 1;
+
+		const { x1, y1 } = tempElem[index];
+		tempElem[index] = createElement(x1, y1, clientX, clientY);
+		setElems(tempElem);
+
+	};
+
+	return (
+		<canvas
+			id="canvas"
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}
+			onMouseMove={handleMouseMove}
+		></canvas>
+	);
+};
 
 export default App;
